907abc28c040101698454c75a3795a137ad75065
João Pedro A <jaaopbr@gmail.com>
2023-07-30 21:15:33 -0300

    fix: auth middleware

--- src/middlewares/ensureAuth.mjs
+++ src/middlewares/ensureAuth.mjs
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import Logger from "../utils/logger.mjs";
 
 export const ensureAuth = async (req, res, next) => {
     try {
@@ -7,12 +8,13 @@ export const ensureAuth = async (req, res, next) => {
         const token = req.headers.authorization.split(" ")[1];
         if (!token) return res.status(403).json({ data: "Malformed authorization header" });
 
-        const payload = await jwt.verify(token, "secret");
+        const payload = await jwt.verify(token, process.env.SECRET);
         if (!payload) return res.status(400).json({ data: "Token verification failed" });
 
         req.user = payload;
         next();
     } catch (e) {
+        Logger.error(e.message);
         res.status(400).json({ data: e.message });
     }
 };
